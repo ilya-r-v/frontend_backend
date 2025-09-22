a59d6ffc7432215be1aeec04b58dddfe2f083bf5
Ilya <rodnov.ilya777@gmail.com>
2025-09-22 11:05:26 +0300

    feat(root): поправлены отступы 2

--- modal.js
+++ modal.js
@@ -11,30 +11,31 @@ openBtn.addEventListener('click', () => {
 });
 closeBtn.addEventListener('click', () => dlg.close('cancel'));
 form?.addEventListener('submit', (e) => {
- // 1) Сброс кастомных сообщений
- [...form.elements].forEach(el => el.setCustomValidity?.(''));
- // 2) Проверка встроенных ограничений
- if (!form.checkValidity()) {
- e.preventDefault();
- alert('Форма успешно отправлена')
- // Пример: таргетированное сообщение
- const email = form.elements.email;
- if (email?.validity.typeMismatch) {
- email.setCustomValidity('Введите корректный e-mail, например name@example.com');
- }
- form.reportValidity(); // показать браузерные подсказки
- // A11y: подсветка проблемных полей
- [...form.elements].forEach(el => {
- if (el.willValidate) el.toggleAttribute('aria-invalid',
-!el.checkValidity());
- });
- return;
- }
- // 3) Успешная «отправка» (без сервера)
- e.preventDefault();
- // Если форма внутри <dialog>, закрываем окно:
- document.getElementById('contactDialog')?.close('success');
- form.reset();
+    // 1) Сброс кастомных сообщений
+    [...form.elements].forEach(el => el.setCustomValidity?.(''));
+    // 2) Проверка встроенных ограничений
+    if (!form.checkValidity()) {
+        e.preventDefault();
+
+        // Пример: таргетированное сообщение
+        const email = form.elements.email;
+        if (email?.validity.typeMismatch) {
+            email.setCustomValidity('Введите корректный e-mail, например name@example.com');
+        }
+        form.reportValidity(); // показать браузерные подсказки
+        // A11y: подсветка проблемных полей
+        [...form.elements].forEach(el => {
+            if (el.willValidate) el.toggleAttribute('aria-invalid',
+                !el.checkValidity());
+        });
+            return;
+    }
+    // 3) Успешная «отправка» (без сервера)
+    e.preventDefault();
+    // Если форма внутри <dialog>, закрываем окно:
+    document.getElementById('contactDialog')?.close('success');
+    alert('Форма успешно отправлена')
+    form.reset();
 });
 dlg.addEventListener('close', () => { lastActive?.focus(); });
 // Esc по умолчанию вызывает событие 'cancel' и закрывает <dialog>
